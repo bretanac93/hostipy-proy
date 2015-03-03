c8450e941982bee8d1760bd390fecf2f76452240
Cesar Bretana <bretana@nauta.cu>
2015-03-03 17:26:13 -0500

    News CRUD finished (issue with delete option, check it later.)

--- src/Hospity/AppBundle/Controller/NoticiasController.php
+++ src/Hospity/AppBundle/Controller/NoticiasController.php
@@ -13,7 +13,7 @@ use Hospity\AppBundle\Form\NoticiaType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-///TODO: Configure the FOSUserBundle correctly, already installed, only configure.
+
 class NoticiasController extends Controller{
 
     public function indexAction() {
@@ -41,6 +41,80 @@ class NoticiasController extends Controller{
         return $this->render('@App/Noticias/create.html.twig', array('formulary' => $form));
     }
 
+    public function detailsAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $new = $em->getRepository('AppBundle:Noticia')->find($id);
+
+        if($new == null) {
+            $this->errorHandling();
+        }
+        else {
+            return $this->render('@App/Noticias/details.html.twig', array('new' => $new));
+        }
+    }
+
+    public function editAction($id) {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AppBundle:Noticia')->find($id);
+        if(!$entity) {
+            $this->errorHandling();
+        }
+
+        $editForm = $this->createEditionForm($entity);
+        return $this->render('@App/Noticias/edit.html.twig', array(
+            'entity' => $entity,
+            'edit_form' => $editForm->createView())
+        );
+
+    }
+
+    public function updateAction($id, Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('AppBundle:Noticia')->find($id);
+
+        if(!$entity) {
+            $this->errorHandling();
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditionForm($entity);
+
+        $editForm->handleRequest($request);
+
+        if($editForm->isValid()) {
+            $em->flush();
+            return $this->redirect($this->generateUrl('news_edit', array('id' => $id)));
+        }
+
+        return $this->render('@App/Noticias/edit.html.twig', array(
+            'entity' => $entity,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    public function deleteAction(Request $request, $id) {
+        $form = $this->createDeleteForm($id);
+        $form -> handleRequest($request);
+
+        if($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('AppBundle:Noticia')->find($id);
+
+
+            if(!$entity) {
+                $this->errorHandling();
+            }
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('news_homepage'));
+    }
+
+
+    ///Forms section
     private function createCreationForm(Noticia $entity) {
         $form = $this->createForm(
             new NoticiaType($this),
@@ -53,4 +127,32 @@ class NoticiasController extends Controller{
 
         return $form;
     }
+
+    private function createEditionForm(Noticia $entity) {
+        $form = $this->createForm(
+            new NoticiaType($this),
+            $entity,
+            array(
+                'action' => $this->generateUrl('news_update', array('id' => $entity->getId())),
+                'method' => 'PUT',
+            )
+        );
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+
+    private function createDeleteForm($id) {
+        $form = $this -> createFormBuilder()
+                      -> setAction($this->generateUrl('news_delete', array('id' => $id)))
+                      -> setMethod('DELETE')
+                      -> add('submit', 'submit', array('label' => 'Delete'));
+
+        return $form->getForm();
+    }
+
+    private function errorHandling(){
+        return $this->render('AppBundle:Default:404.html.twig');
+    }
+
 } 
